3f910f8fa57a124b46bfb1013e9f9eca72a18905
iKyleCrane <21-se-30@students.uettaxila.edu.pk>
2025-11-11 14:34:04 +0500

    sending the received data to the bitrix local application

--- controllers/ProcessTheIncomingMessagesFromWhatsapp.js
+++ controllers/ProcessTheIncomingMessagesFromWhatsapp.js
@@ -1,7 +1,13 @@
+import { broadcast } from '../websocket/connections.js';
+
+
+
 export default async function ProcessTheIncomingMessages(req, res) {
   try {
     // Log the incoming message
     console.log('Incoming Message Data:', JSON.stringify(req.body, null, 2));
+
+    broadcast({ type: 'NEW_MESSAGE', data: req.body });
 
     // Send a simple response
     res.status(200).send('Message received');
